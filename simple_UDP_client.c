a29ecb8b9b2214936d0a5277e996b630130dffe5
FrankFan37 <87948495+FrankFan37@users.noreply.github.com>
2023-06-14 16:32:44 -0400

    Add files via upload

---
+++ simple_UDP_client.c
@@ -0,0 +1,74 @@
+/*
+Simple UDP client based on Silver Moon's tutorial
+https://www.binarytides.com/programming-udp-sockets-c-linux/
+*/
+#include<stdio.h>	//printf
+#include<string.h> //memset
+#include<stdlib.h> //exit(0);
+#include<arpa/inet.h>
+#include<sys/socket.h>
+#include<unistd.h>  // close()
+
+#define SERVER "127.0.0.1"
+#define BUFLEN 512	//Max length of buffer
+#define PORT 8888	//The port on which to send data
+
+void die(char *s)
+{
+	perror(s);
+	exit(1);
+}
+
+/*
+flow of code: socket() -> sendto()/recvfrom()
+
+*/
+
+int main(void)
+{
+	struct sockaddr_in si_other;
+	int s, i, slen=sizeof(si_other);
+	char buf[BUFLEN];
+	char message[BUFLEN];
+
+	if ( (s=socket(AF_INET, SOCK_DGRAM, IPPROTO_UDP)) == -1)
+	{
+		die("socket");
+	}
+
+	memset((char *) &si_other, 0, sizeof(si_other));
+	si_other.sin_family = AF_INET;
+	si_other.sin_port = htons(PORT);
+	
+	if (inet_aton(SERVER , &si_other.sin_addr) == 0) 
+	{
+		fprintf(stderr, "inet_aton() failed\n");
+		exit(1);
+	}
+
+	while(1)
+	{
+		printf("Enter message : ");
+		gets(message); // ???
+		
+		//send the message
+		if (sendto(s, message, strlen(message)+1 , 0 , (struct sockaddr *) &si_other, slen)==-1) // strlen +1 for string null terminator
+		{
+			die("sendto()");
+		}
+		
+		//receive a reply and print it
+		//clear the buffer by filling null, it might have previously received data
+		memset(buf,'\0', BUFLEN);
+		//try to receive some data, this is a blocking call
+		if (recvfrom(s, buf, BUFLEN, 0, (struct sockaddr *) &si_other, &slen) == -1)
+		{
+			die("recvfrom()");
+		}
+		
+		puts(buf);
+	}
+
+	close(s);
+	return 0;
+}
---
+++ simple_UDP_server.c
@@ -0,0 +1,131 @@
+/*
+Simple udp server based on Silver Moon's tutorial
+https://www.binarytides.com/programming-udp-sockets-c-linux/
+
+*/
+
+#include<stdio.h>	//printf
+#include<string.h>  //memset
+#include<stdlib.h>  //exit(0);
+#include<arpa/inet.h>
+#include<sys/socket.h>
+#include<unistd.h>  // close()
+
+// hold data being sent or received over a network socket
+// acts as a temporary storage area for the data being transferred
+#define BUFLEN 512	//Max length of buffer
+
+#define PORT 8888	//The port on which to listen for incoming data, identifies a specific process
+
+void die(char *s)
+{
+	perror(s);
+	exit(1);
+}
+
+/*
+flow of code: socket() -> bind() -> recvfrom() -> sendto()
+
+*/
+int main(void)
+{
+    // data structure representing an internet address
+    /*
+    definition of the sockaddr_in structure:
+        struct sockaddr_in 
+        {
+            sa_family_t sin_family;  // Address family (typically AF_INET for IPv4 addresses)
+            in_port_t sin_port;      // Port number in network byte order
+            struct in_addr sin_addr; // IP address in network byte order
+            char sin_zero[8];        // Padding to match the size of struct sockaddr
+        };
+
+    */
+    struct sockaddr_in si_me, si_other; // si_other: sender
+	
+	int s, i, slen = sizeof(si_other) , recv_len;
+	char buf[BUFLEN];
+	
+	//create a UDP(User Datagram Protocol) socket (datagram sockets; not connection oriented, unreliable communication); another type is TCP
+
+    /*
+    socket: serves as an endpoint for communication between processe/applications within a network
+
+    socket(domain, type, protocol): taking 3 arguments to create a new socket:
+        1. specifies the address family or domain of the socket; AF_INET for IPv4 addresses
+        2. specifies the type of the socket; SOCK_DGRAM indicates a datagram socket
+        3. specifies the protocol to be used; IPPROTO_UDP indicates that the UDP protocol will be used
+    
+    */
+	if ((s=socket(AF_INET, SOCK_DGRAM, IPPROTO_UDP)) == -1)
+	{
+		die("socket");
+	}
+
+	
+	// zero out the structure
+	memset((char *) &si_me, 0, sizeof(si_me));
+	
+	si_me.sin_family = AF_INET;
+
+    // convert to network byte order using htons()
+        // convert a 16-bit unsigned short integer from host byte order
+	si_me.sin_port = htons(PORT); 
+
+    // sin_addr stores the IPv4 address. 
+    // It is of type struct in_addr, which itself contains a single member s_addr representing the IP address in network byte order.
+        // convert a 32-bit unsigned integer from host byte order
+	si_me.sin_addr.s_addr = htonl(INADDR_ANY);
+
+    	
+	// bind socket to port; binds a socket, specified by the socket descriptor s, to a specific address and port number.
+    // bind the socket to the local address specified by the si_me struct 
+
+    // This allows the socket to listen for incoming connections or send data from the specified local address and port.
+	if( bind(s , (struct sockaddr*)&si_me, sizeof(si_me) ) == -1) // ???
+	{
+		die("bind");
+	}
+
+
+
+    //keep listening for data
+    while(1)
+	{
+		printf("Waiting for data...");
+		fflush(stdout);  // clear the output buffer ???
+		
+		//try to receive some data, this is a blocking call
+
+        // recvfrom(): receive data from a socket. It reads data from the socket specified by the socket descriptor s and stores it in the buffer buf.
+
+		if ((recv_len = recvfrom(s, buf, BUFLEN, 0, (struct sockaddr *) &si_other, &slen)) == -1) // 0: optional flag - can modify recvfrom's behavior
+		{
+			die("recvfrom()");
+		}
+		
+		//print details of the client/peer and the data received
+
+        /*
+            inet_ntoa(): converts an IPv4 address from binary form to a human-readable string format
+
+            ntohs(): convert a 16-bit unsigned short integer from network byte order to host byte order
+        */
+		printf("Received packet from %s:%d\n", inet_ntoa(si_other.sin_addr), ntohs(si_other.sin_port));
+		printf("Data: %s\n" , buf);
+		
+		//now reply the client with the same data
+		if (sendto(s, buf, recv_len, 0, (struct sockaddr*) &si_other, slen) == -1)
+		{
+			die("sendto()");
+		}
+	}
+
+	close(s);
+	return 0;
+
+
+
+
+
+}
